1dad4047fff6187e543b7c543c68e1b5a443810e
Konrad <konradbieniek@gmail.com>
2023-07-08 23:12:02 +0200

    successfull connected to mailchimp API

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import https from "https"
 import "dotenv/config"
 import path from "path"
 import { fileURLToPath } from "url"
@@ -8,7 +9,10 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
+
 const apiKey = process.env.API_KEY
+const listID = process.env.LIST_ID
+const server = process.env.SERVER
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -16,17 +20,42 @@ app.use(express.static("public"))
 
 app.get("/", (req, res) => {
 	res.sendFile(`${__dirname}/signup.html`)
-	console.log(apiKey)
 })
 
 app.post("/", (req, res) => {
-	const response = {
+	const reqDataObj = {
 		fName: req.body.firstName,
-		sName: req.body.secondName,
+		lName: req.body.lastName,
 		email: req.body.email,
 	}
-	// const response = req.body.firstName
-	console.log(`${response.fName} ${response.sName} ${response.email}`)
+
+	const data = {
+		members: [
+			{
+				email_address: reqDataObj.email,
+				status: "subscribed",
+				merge_fields: {
+					FNAME: reqDataObj.fName,
+					LNAME: reqDataObj.lName,
+				},
+			},
+		],
+	}
+
+	const jsonData = JSON.stringify(data)
+	const url = `https://us${server}.api.mailchimp.com/3.0/lists/${listID}`
+	const options = {
+		method: "POST",
+		auth: `Pan.Be:${apiKey}`,
+	}
+
+	const request = https.request(url, options, (response) => {
+		response.on("data", (data) => {
+			console.log(JSON.parse(data))
+		})
+	})
+	request.write(jsonData)
+	request.end()
 })
 
 app.listen(3000, () => console.log("server is running on port 3000."))
